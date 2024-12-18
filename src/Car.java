5af582ad09894d3f71d35558b7554ed3e11e5d66
Arianna Johnson <ariannajohnson588@gmail.com>
2024-12-17 21:16:16 -0500

    OOP fields

--- src/Car.java
+++ src/Car.java
@@ -3,11 +3,57 @@
 // and not enclosed in the code block of another class, type or method
 
 public class Car {
+    // Fields in java are assigned default values
+    // (boolean: false, numeric types: 0, any other type: null)
     private String make;
     private String model;
     private String color;
     private int doors;
     private boolean convertible;
+
+    // Getters & Setters (used to control & protect access of private fields)
+    // a getter - retrieves the value of a private field and returns it
+    // a setter - sets the value of a private field
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
 
     public void describeCar() {
         System.out.println(doors + "-Door " +
