098fa3b1bf5bcc6b627ae4738e1c1ef820b6b29f
Arianna Johnson <ariannajohnson588@gmail.com>
2024-12-22 12:21:30 -0500

    day 6: more inheritance

--- src/Inheritance/Dog.java
+++ src/Inheritance/Dog.java
@@ -2,9 +2,34 @@ package Inheritance;
 
 // Using `extends` specifies the superclass or the parent class
 public class Dog extends Animal {
+    private String earShape;
+    private String tailShape;
     public Dog() {
         // super() is a lot like this
         // `super` is a way to call the constructor of a superclass from the subclass
         super("Mutt", "Big", 300);
+    }
+
+    public Dog(String type, double weight) {
+        this(type, weight, "Perky", "Curled");
+    }
+
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small": "large", weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                "} " + super.toString();
+    }
+
+    // Overrides the Animal classes make noise method when called on a Dog instance
+    // Overriding a method is when you create a method on a subclass that has the same signature as the superclass
+    // You override a parent class when you want the subclass to have different behavior for that method
+    public void makeNoise() {
     }
 }
--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -9,6 +9,13 @@ public class Main {
         // we can pass a dog instance to any method that takes an animal type because
         // the Dog class is a subclass of the Animal class
         doAnimalStuff(dog, "fast");
+
+        Dog yorkie = new Dog("Yorkie", 14);
+        doAnimalStuff(yorkie, "fast");
+
+        Dog retriever = new Dog("Labrador Retriever", 64, "Floppy", "Swimmer");
+        doAnimalStuff(retriever, "slow");
+
     }
 
     public static void doAnimalStuff(Animal animal, String speed) {
